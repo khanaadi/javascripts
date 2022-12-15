c1383bb9bc67eb236c76de879fdce433b59609ff
Aadil khan <p.aadilkhan7386@gmail.com>
2022-12-15 16:50:18 +0530

    Add to 2-data-structure

---
+++ 2-data-structure/3-object.js
@@ -0,0 +1,10 @@
+({
+    "name": "Aadil khan",
+    designation: "Backend Developer",
+    friends: ["afroz", "sameer", "Razak"],
+    company: {
+      name: "Trangla",
+      position: "Backend Dev"
+    },
+    "full name": "Pathan Aadil Khan"
+  })["company"]
